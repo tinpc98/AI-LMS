0f12f128b7602dcf01ad4d7893a7eca7ce83be0f
GiaBaoNguyen <ngb310305@gmail.com>
2026-07-07 16:06:58 +0700

    routes Teacher

--- Frontend/src/components/layout/SidebarTeacher.tsx
+++ Frontend/src/components/layout/SidebarTeacher.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SidebarTeacher() {
   return (
@@ -10,30 +11,26 @@ export default function SidebarTeacher() {
           src="https://lh3.googleusercontent.com/aida-public/AB6AXuCkHipXsG3Jdqn_T1kn-iQLl2Rgp0xI8NBtwkLr3AVXSNtxKGcQtd_vjkQf3Nqy4bvn2cdUDnYhl8CrLatmVuOJEc5Thz15ltoUa3CDz-PJd-0j8e0eg2tskVHGYfd6MxAJBXUBxYSLxZY2TMqb-zZRzRpW7jDDO6GFnwr5QE5Ic2nWHSb7TS-VEM406cwmWV4b0vXD2nI0KEFO8c4sBMI-D2I-NJIt6KIo6522qWXOGZGyfHdA9l2oEGClirojk5JPXdilCbLLyPXS"
         />
         <div>
-          <h1 className="font-headline-md text-headline-md font-bold text-primary">
-            EduPortal AI
-          </h1>
-          <p className="font-body-sm text-body-sm text-on-surface-variant">
-            Teacher Dashboard
-          </p>
+          <h1 className="font-headline-md text-headline-md font-bold text-primary">EduPortal AI</h1>
+          <p className="font-body-sm text-body-sm text-on-surface-variant">Teacher Dashboard</p>
         </div>
       </div>
 
       <div className="mt-4 px-4 space-y-1 flex-grow">
-        <a
+        <Link
+          to="/teacher"
           className="flex items-center gap-3 px-4 py-3 bg-secondary-container text-on-secondary-container font-bold rounded-lg transition-transform duration-100 active:scale-95"
-          href="#"
         >
           <span className="material-symbols-outlined">dashboard</span>
-          <span className="font-label-md text-label-md">Dashboard</span>
-        </a>
-        <a
+          <span className="font-label-md text-label-md">Trang chủ</span>
+        </Link>
+        <Link
+          to="classroom-management"
           className="flex items-center gap-3 px-4 py-3 text-on-surface-variant hover:bg-surface-container-low rounded-lg transition-all duration-200"
-          href="#"
         >
           <span className="material-symbols-outlined">groups</span>
           <span className="font-label-md text-label-md">Quản lý lớp học</span>
-        </a>
+        </Link>
         <a
           className="flex items-center gap-3 px-4 py-3 text-on-surface-variant hover:bg-surface-container-low rounded-lg transition-all duration-200"
           href="#"
