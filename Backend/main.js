b3ec038f694e4d90b268a22bd688f5552eebb94c
GiaBaoNguyen <ngb310305@gmail.com>
2026-07-08 21:19:33 +0700

    fix bug connectDB

--- Backend/main.js
+++ Backend/main.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import dns from "dns";
 import cors from "cors"; // Thêm thư viện cấu hình cho phép Frontend gọi API
 import { connectDB } from "./src/config/database.js";
 import UserRouter from "./src/routers/user.routes.js";
 import ClassRouter from "./src/routers/class.router.js";
 import LessonRouter from "./src/routers/lesson.routes.js";
 
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 // Kích hoạt cấu hình file .env
 dotenv.config();
 
@@ -14,9 +16,7 @@ const port = process.env.PORT || 5000; // Linh hoạt lấy port từ .env, nế
 
 // Lấy danh sách origin từ biến môi trường FRONTEND_ORIGINS (comma-separated).
 // Ví dụ: FRONTEND_ORIGINS=http://localhost:5173,http://localhost:5174
-const allowedOrigins = (
-  process.env.FRONTEND_ORIGINS || "http://localhost:5173,http://127.0.0.1:5173"
-)
+const allowedOrigins = (process.env.FRONTEND_ORIGINS || "http://localhost:5173,http://127.0.0.1:5173")
   .split(",")
   .map((u) => u.trim())
   .filter(Boolean);
@@ -43,16 +43,12 @@ app.use("/api/auth", UserRouter);
 app.use("/api/classes", ClassRouter);
 app.use("/api/lesson", LessonRouter);
 app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Server EduSynth AI đang hoạt động ổn định!" });
+  res.status(200).json({ message: "Server EduSynth AI đang hoạt động ổn định!" });
 });
 
 // Xử lý lỗi 404 cho các đường dẫn không tồn tại
 app.use((req, res) => {
-  res
-    .status(404)
-    .json({ message: "Đường dẫn API này không tồn tại trên hệ thống!" });
+  res.status(404).json({ message: "Đường dẫn API này không tồn tại trên hệ thống!" });
 });
 
 connectDB()
--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
+const uri = process.env.MONGO_URI;
+
 export const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI;
+    const uri = "mongodb+srv://admin:admin123@cluster0.wissmyr.mongodb.net/AI-LMS?appName=Cluster0";
     if (!uri) {
       console.log(" Không tìm thấy MONGO_URI trong file .env");
     }
-
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, { serverSelectionTimeoutMS: 10000, family: 4, tls: true });
     console.log("Kết nối MongoDB thành công");
   } catch (error) {
-    console.log("Kết nối thất bại:", error.message);
-    throw error; // Bắt buộc ném lỗi ra ngoài để main.js dừng khởi động server
+    console.log("Kết nối thất bại:", error);
+    throw error;
   }
 };
