dca069a758e85e052a16de748b8e9a49be49ffef
GiaBaoNguyen <ngb310305@gmail.com>
2026-07-26 23:41:14 +0700

    Fix navigate theo role và reaload login

--- Frontend/src/components/common/PublicRoute.tsx
+++ Frontend/src/components/common/PublicRoute.tsx
@@ -6,10 +6,13 @@ export default function PublicRoute() {
 
   // Nếu người dùng đã đăng nhập và có Token + Role hợp lệ
   if (token && role) {
+    if (role === "admin") {
+      return <Navigate to="/admin" replace />;
+    }
     if (role === "teacher") {
       return <Navigate to="/teacher" replace />;
     }
-    if (role === "student" || role === "admin") {
+    if (role === "student") {
       return <Navigate to="/" replace />;
     }
   }
